9a0ad1da8e16989ced9d18651dd021e6ac95af2a
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-03-06 16:46:07 -0800

    Created inventory object

--- inventory.js
+++ inventory.js
@@ -3,41 +3,83 @@ class Inventory {
     constructor() {
         this.player; //assign each inventory object to a player
         this.TR = 5; //starting value=5 instead of 20
-        this.domElement; //stores DOM element created by render method
-        //The following will have buttons, greyed out if unavailable (false) and clickable if available (true)
-        this.canSellSteel = false; //changes to true if steel amount >= 1 (and actions are available)
-        this.canSellTitanium = false; //changes to true if titanium amount >= 1 (and actions are available)
-        this.canConvertPlants = false; //changes to true if plant amount >= 8 (and actions are available)
-        this.canConvertHeat = false; //changes to true if heat amount >= 8 (and actions are available)
+        this.resources = ['money', 'steel', 'titanium', 'plants', 'energy', 'heat'];
+        this.inventoryDomElement; //stores DOM element created by render method
+        this.resourceTrackers = [];
     }
+
     createTrackers() {
-        //create 6 tracker objects, one for each resource
-        //append them to the rendered inventory DOM element
+        for (var resourceIndex = 0; resourceIndex < this.resources.length; resourceIndex++) {
+            var tracker = new ResourceTracker(this.resources[resourceIndex]);
+            this.resourceTrackers.push(tracker);
+            var trackerDomElement = tracker.render();
+            this.inventoryDomElement.append(trackerDomElement);
+        }
     }
-    render() {
-        //creates a DOM element to hold the current object
-        //returns it as this.domElement
-        //must be called before createTrackers, so trackers have somewhere to go
+
+    render(numberOfPlayers) { //move to game object
+        for (var playerIndex = 1; playerIndex <= numberOfPlayers; playerIndex++) {
+            this.inventoryDomElement = $('<div>').addClass('inventory');
+            $('.players-display').append(this.inventoryDomElement);
+            return this.inventoryDomElement;
+        }
     }
-    sellSteel(amount) {
-        //takes in an amount of steel to be sold and removes from the steel tracker bank (not production)
-        //multiplies amount x2 and adds that much money to bank
-        //counts as one action
+
+    getAmount(resource) { //takes in a number; ex: 0 for money, 1 for steel.... 5 for heat
+        return this.resourceTrackers[resource].amount;
     }
-    sellTitanium(amount) {
-        //takes in an amount of titanium to be sold and removes from the titanium tracker bank
-        //multiplies amount x3 and adds that much money to bank
-        //counts as one action
+
+    checkResources() { //move to player object
+        //runs when 'Conversions' button (person object) is clicked
+        //convert buttons are greyed out by default, aka .disabled=true
+        if (getAmount(1) >= 1 && actionsAvailable) {
+            $('.sellSteelButton').disabled = false;
+        }
+        if (getAmount(2) >= 1 && actionsAvailable) {
+            $('.sellTitaniumButton').disabled = false;
+        }
+        if (getAmount(3) >= 8 && actionsAvailable) {
+            $('.convertPlantsButton').disabled = false;
+        }
+        if (getAmount(5) >= 8 && actionsAvailable) {
+            $('.convertHeatButton').disabled = false;
+        }
     }
-    convertPlants() {
-        //removes 8 plants from plant bank
-        //gives player a greenery tile and allows them to place it on any open space
-        //increases oxygen by one step
-        //increases player's TR by 1
+
+    sellSteel() { //move to player object
+        //runs when .sellSteelButton is clicked
+        var numberToSell; //get number to sell from user input
+        if (numberToSell <= getAmount(1)) {
+            this.resourceTrackers[1].amount -= numberToSell;
+            this.resourceTrackers[0].amount += 2*numberToSell;
+            actionsAvailable--;
+        }
     }
-    convertHeat() {
-        //removes 8 heat from heat bank
-        //increases temp by one step
-        //increases player's TR by 1
+
+    sellTitanium() { //move to player object
+        //runs when .sellTitaniumButton is clicked
+        var numberToSell; //get number to sell from user input
+        if (numberToSell <= getAmount(2)) {
+            this.resourceTrackers[2].amount -= numberToSell;
+            this.resourceTrackers[0].amount += 3*numberToSell;
+            actionsAvailable--;
+        }
+    }
+
+    convertPlants() { //move to player object
+        //runs when .convertPlantsButton is clicked
+        this.resourceTrackers[3].amount -= 8;
+        this.TR++;
+        actionsAvailable--;
+        //also needs to give player a greenery tile and allow them to place it on any open space
+        //also needs to increase oxygen by one step
+    }
+
+    convertHeat() { //move to player object
+        //runs when .convertHeatButton is clicked
+        this.resourceTrackers[5].amount -= 8;
+        this.TR++;
+        actionsAvailable--;
+        //also needs to increase temp by one step
     }
 }
